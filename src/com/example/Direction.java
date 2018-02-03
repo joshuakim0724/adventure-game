d86d4d3797a311dabc0dc4055c9a7cf7b42d5eec
joshuakim0724 <joshuakim0724@gmail.com>
2018-02-03 00:15:10 -0600

    Created the Direction class to help setup

---
+++ src/com/example/Direction.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Direction {
+    private String directionName;
+    private String name;
+
+    public String getDirectionName() {
+        return directionName;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
