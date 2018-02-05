01fd99142cfc3dd09107421db8e38d139c74650f
joshuakim0724 <joshuakim0724@gmail.com>
2018-02-04 23:06:18 -0600

    Adventure can read the json link and create an object using json

--- src/com/example/Adventure.java
+++ src/com/example/Adventure.java
@@ -1,50 +1,88 @@
 package com.example;
+
+import com.google.gson.Gson;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+
 import java.util.Scanner;
-import com.google.gson.Gson;
 
 public class Adventure {
     private static URL url;
-    private static AdventureSetup setup;
+    private static AdventureSetup adventureSetup;
     private static boolean isFinished = false;
     private static Room[] rooms;
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Gson gson = new Gson();
+        /**
+         * https://github.com/zillesc/WashingtonPost
+         * https://echo360.org/lesson/G_0bf45e48-dfa7-488a-88db-4699e6468c8d_b0113c19
+         * -4871-4da9-bad8-9a5f6b017a2f_2018-02-01T12:30:00.000_2018-02-01T13:57:00.000/classroom#sortDirection=desc
+         * Classroom video link
+         * Looked at the lecture in class to figure out how to get a Json from a URL
+         *
+         * //https://stackoverflow.com/questions/31504123/unhandled-exception-java-net-malformedurlexception
+         */
+        try {
+            final HttpResponse<String> stringHttpResponse;
 
-        try { //https://stackoverflow.com/questions/31504123/unhandled-exception-java-net-malformedurlexception
             url = new URL(AdventureURL.JSON_LINK);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        setup = gson.fromJson(url.toString(), AdventureSetup.class);
 
-        while (!isFinished) {
-            System.out.println("Your starting room is: " + setup.getStartingRoom());
-            rooms = setup.getRooms();
-            for (Room name : rooms) {
-                if (name.getName().equals(setup.getStartingRoom())) {
-                    System.out.println(name.getDescription());
-                    System.out.println("Your journey beings here");
-                    if (name.getItems().length == 0) {
-                        System.out.println("This room contains nothing");
-                    } else {
-                        System.out.
+            stringHttpResponse = Unirest.get(AdventureURL.JSON_LINK).asString();
+            // Check to see if the request was successful; if so, convert the payload JSON into Java objects
+
+            String json = stringHttpResponse.getBody();
+            AdventureSetup adventureSetup = gson.fromJson(json, AdventureSetup.class);
+
+            //while (!isFinished) {
+                System.out.println("Your starting room is: " + adventureSetup.getStartingRoom());
+                rooms = adventureSetup.getRooms();
+                for (Room name : rooms) {
+                    if (name.getName().equals(adventureSetup.getStartingRoom())) {
+                        System.out.println(name.getDescription());
+                        System.out.println("Your journey beings here");
+                        if (name.getItems().length == 0) {
+                            System.out.println("This room contains nothing");
+                        } else {
+                            System.out.print("This room contains ");
+                            for (int i = 0; i < name.getItems().length; i++) {
+                                if (i == name.getItems().length - 1) {
+                                    System.out.println(name.getItems()[i]);
+                                } else {
+                                    System.out.print(name.getItems()[i] + ", ");
+                                }
+                            }
+                        }
                     }
                 }
-            }
-        }
 
-        // this is a 'for each' loop; they are useful when you want to do something to
-        // every element of a collection and you don't care about the index of the element
-        for (String arg : args) {
-            System.out.print("\"" + arg + "\" ");
+        } catch (UnirestException e) {
+            e.printStackTrace();
+            System.out.println("Network not responding");
+        } catch (MalformedURLException e) {
+            System.out.println("Bad URL: " + url);
         }
+    }
+    //Don't actually use this, but have this to reference Zillecs code that he showed
+    static void makeApiRequest(String url) throws UnirestException, MalformedURLException {
+        final HttpResponse<String> stringHttpResponse;
 
-        System.out.println("");
-        // you can remove this code; it is just illustrating the use of arguments.
+        // This will throw MalformedURLException if the url is malformed.
+        new URL(AdventureURL.JSON_LINK);
+
+        stringHttpResponse = Unirest.get(url).asString();
+        // Check to see if the request was successful; if so, convert the payload JSON into Java objects
+        if (stringHttpResponse.getStatus() == 200) {
+            String json = stringHttpResponse.getBody();
+            Gson gson = new Gson();
+            final AdventureSetup adventureSetup = gson.fromJson(json, AdventureSetup.class);
+        }
     }
 }
