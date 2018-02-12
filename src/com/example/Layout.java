7553a22593fb4e9e27074edfe2eed25de161b52e
joshuakim0724 <joshuakim0724@gmail.com>
2018-02-11 21:04:03 -0600

    Added Item, Monster, Room class and renamed AdventureSetup to Layout (Layout/player didn't commit for some reason) so recommitting

---
+++ src/com/example/Layout.java
@@ -0,0 +1,26 @@
+package com.example;
+
+public class Layout {
+
+    private String startingRoom;
+    private String endingRoom;
+    private Room[] rooms;
+
+    public String getStartingRoom() {
+        return startingRoom;
+    }
+
+    public void setStartingRoom(String room) {
+        startingRoom = room;
+    }
+
+    public String getEndingRoom() {
+        return endingRoom;
+    }
+
+    public Room[] getRooms() {
+        return rooms;
+    }
+
+
+}
